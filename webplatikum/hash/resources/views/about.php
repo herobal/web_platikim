ed43ea633704d66e10204531e8ed674559dc7ad2
ikadekmahaganggaE <93956310+herobal@users.noreply.github.com>
2022-10-11 00:57:18 +0000

    adabaru

---
+++ webplatikum/hash/resources/views/about.php
@@ -0,0 +1,134 @@
+<html>
+    <head>
+        <!-- Basic -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Mobile Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Site Metas -->
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Lodge</title>
+
+        <!-- slider stylesheet -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css">
+
+        <!-- bootstrap core css -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+        <!-- fonts style -->
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Poppins:400,600,700&amp;display=swap" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- responsive style -->
+        <link href="css/responsive.css" rel="stylesheet">
+        </head>
+
+        <body class="sub_page">
+
+        <div class="hero_area">
+            <!-- header section strats -->
+            <header class="header_section">
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container ">
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/logo.png" alt="">
+                    <span>
+                    Lodge
+                    </span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                    <ul class="navbar-nav  ">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="about.html"> About</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="jewellery.html">Jewellery </a>
+                        </li>
+                        
+                        
+                    </ul>
+
+                    </div>
+                    
+                </div>
+                </nav>
+            </div>
+            </header>
+            <!-- end header section -->
+        </div>
+
+
+
+        <!-- about section -->
+
+        <section class="about_section layout_padding2-top layout_padding-bottom">
+            <div class="design-box">
+            <img src="images/design-2.png" alt="">
+            </div>
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                <div class="detail-box">
+                    <div class="heading_container">
+                    <h2>
+                        About Form Register
+                    </h2>
+                    </div>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrudLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud
+                    </p>
+                    <div>
+                    <a href="">
+                        Home
+                    </a>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-6">
+                <div class="img-box">
+                    <img src="images/about-img.png" alt="">
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+        <!-- end about section -->
+
+
+        <!-- info section -->
+        
+
+        <!-- end info_section -->
+
+        <!-- footer section -->
+        <section class="container-fluid footer_section">
+            <p>
+            © <span id="displayYear">2022</span> All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </section>
+        <!-- footer section -->
+
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
+
+
+
+    </body>
+</html>
---
+++ webplatikum/hash/resources/views/welcome.blade.php
@@ -0,0 +1,181 @@
+<html>
+    <head>
+        <!-- Basic -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Mobile Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Site Metas -->
+        <meta name="keywords" content="tst">
+        <meta name="description" content="test">
+        <meta name="author" content="MAHA_GANGGA">
+
+        <title>MAHA_GANGGA</title>
+
+        <!-- slider stylesheet -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css">
+
+        <!-- bootstrap core css -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+        <!-- fonts style -->
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Poppins:400,600,700&amp;display=swap" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- responsive style -->
+        <link href="css/responsive.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg custom_nav-container ">
+            <a class="navbar-brand" href="index.html">
+                <img src="images/logo.png" alt="">
+                <span>
+                MAHA_GANGGA
+                </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                <ul class="navbar-nav  ">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.html"> About</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="jewellery.html">Form register</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
+        </div>
+        <section class=" slider_section position-relative">
+        <div class="design-box">
+            <img src="images/design-1.png" alt="">
+        </div>
+        <div class="slider_number-container d-none d-md-block">
+            
+        </div>
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            
+            <div class="carousel-inner">
+                <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-6">
+                    <div class="detail_box">
+                        <h2>
+                        <span>Selamat Datang</span>
+                        <hr>
+                        </h2>
+                        <h1>
+                        Form Student
+                        </h1>
+                        <p>
+                            website ini di pergunakan untuk mendaftarkan mahasiswa ke dalam kampus
+                        </p>
+                        <div>
+                        <a href="">Daftar Form Student</a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-6">
+                    <div class="img-box">
+                        <img src="images/slider-img.png" alt="">
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-6">
+                    <div class="detail_box">
+                        <h2>
+                        <span> Selamat Datang</span>
+                        <hr>
+                        </h2>
+                        <h1>
+                        Form Student
+                        </h1>
+                        <p>
+                            website ini di pergunakan untuk mendaftarkan mahasiswa ke dalam kampus
+                        </p>
+                        <div>
+                        <a href="">Daftar Form Student</a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-6">
+                    <div class="img-box">
+                        <img src="images/slider-img.png" alt="">
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-6">
+                    <div class="detail_box">
+                        <h2>
+                        <span> Selamat Datang</span>
+                        <hr>
+                        </h2>
+                        <h1>
+                        Form Student
+                        </h1>
+                        <p>
+                        website ini di pergunakan untuk mendaftarkan mahasiswa ke dalam kampus
+                        </p>
+                        <div>
+                        <a href="">Daftar Form Student</a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-6">
+                    <div class="img-box">
+                        <img src="images/slider-img.png" alt="">
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        </section>  
+        </header>
+        <!-- end header section -->
+        <!-- slider section -->
+        <section class=" slider_section position-relative">
+        <div class="design-box">
+            <img src="images/design-1.png" alt="">
+        </div>
+        </section></div>
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">01</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1">02</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
+            </ol>
+            </div>
+        
+        </div>
+        <section class="about_section layout_padding2-top layout_padding-bottom">
+            <div class="design-box">
+      <img src="images/design-2.png" alt="">
+            </div>
+        </section>
+    </body>
+</html>
